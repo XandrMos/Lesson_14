583fd5062c2ea5ca45c7662d24893493d6f5afee
Mosiiuk A <mosxandrwork@gmail.com>
2019-09-16 21:20:34 +0300

    Modify py file

--- Sample_Life.py
+++ Sample_Life.py
@@ -20,6 +20,7 @@ class Life():
                 [0,0,0,0,0,0,0,0,0,0]
                 ]
         self.rnd = 0
+        self.history = []
     def reset(self):
         self.pole = [
                 [0,0,0,0,0,0,0,0,0,0],
@@ -38,16 +39,23 @@ class Life():
         if not (2 <= N <= 100):
             print("Enter 2 <= N <= 100")
             return None
+        self.history.append(list())
         for i in range(N):
-            x = random.randint(0, 9)
-            y = random.randint(0, 9)
-            self.pole[x][y] = 1
-            while self.pole[x][y] != 1:
+            while True:
                  x = random.randint(0, 9)
                  y = random.randint(0, 9)
-                 self.pole[x][y] = 1
+                 if self.pole[x][y] != 1:
+                     self.pole[x][y] = 1
+                     self.history[self.rnd].append((x,y))
+                     break
+        
         for i in range(10):
-            print(self.pole[i])
+            for j in range(10):
+                if self.pole[i][j] == 1:
+                    print('*', end = "")
+                else:
+                    print('_', end = "")
+            print()
         return None
     def count_neighbors(self, x, y):
         sum = 0
@@ -84,16 +92,21 @@ class Life():
                     new_pole[i][j] = 1
                 elif self.pole[i][j] == 1 and cnt in (2, 3):
                     new_pole[i][j] = 1
-                elif self.pole[i][j] == 1 and cnt < 2:
-                    new_pole[i][j] = 0
-                elif self.pole[i][j] == 1 and cnt > 3:
-                    new_pole[i][j] = 0
-                else:
-                    new_pole[i][j] = new_pole[i][j]
+#                elif self.pole[i][j] == 1 and cnt < 2:
+#                    new_pole[i][j] = 0
+#                elif self.pole[i][j] == 1 and cnt > 3:
+#                    new_pole[i][j] = 0
+#                else:
+#                    new_pole[i][j] = new_pole[i][j]
         self.pole = new_pole
-        print()
+        print("Step = ", self.rnd)
         for i in range(10):
-            print(self.pole[i])
+            for j in range(10):
+                if self.pole[i][j] == 1:
+                    print('*', end = "")
+                else:
+                    print('_', end = "")
+            print()
         return None
     def step(self, N = 1):
         for i in range(N):
